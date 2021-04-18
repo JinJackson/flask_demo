5c1dad3fd36cacd3be184628a64c0014fe53c499
Jackson <635725626@qq.com>
2021-04-18 11:58:25 +0800

    modified

--- flask_test_demo.py
+++ flask_test_demo.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 # 2.创建Flask应用程序实例，固定传入__name__，作用是为了确定资源所在路径
 app = Flask(__name__)
 
-# 3.通过装饰器定义路由
+# 3.通过装饰器定义路由, 默认都是post请求
 @app.route('/')
 def hello_world():
     return 'Hello World'
@@ -13,11 +13,28 @@ def hello_world():
 def test():
     return 'test'
 
-#返回一个网页
+# 返回一个网页
 @app.route('/website')
 def testweb():
     return render_template('index.html')  #默认就在templates的目录下
 
+# 使用post请求
+@app.route('/postTest', methods=['POST'])
+def testPost():
+    return 'This is a test for post'
+
+
+# 使用同一个视图参数，显示不同用户的信息
+# <>定义路由参数
+@app.route('/orders/<order_id>')
+def get_order_id(order_id):  #参数名和路由参数同名
+    return 'order_id %s' % order_id
+
+# 有时，希望对参数类型有所限制
+# 将参数强转int
+@app.rout('/orders2/<int:order_id>')
+def get_order_id2(order_id):
+    return 'order_id %s' % order_id
 
 if __name__ == '__main__':
     # 4.启动程序，flask会提供一个简易的本地服务器
